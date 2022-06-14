b8acd15c3bfa32f0c165fc2ed18bac3f35d794f1
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2022-06-14 10:33:09 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+require('dotenv').config();
+const { dbConnection } = require('./database/config');
+
+// Create a new express application instance
+const app = express();
+
+//DB Connection
+dbConnection();
+
+// Public
+app.use(express.static('public'));
+
+// Read and parse the request body
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+
+
+// The port the express app will listen on
+app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
